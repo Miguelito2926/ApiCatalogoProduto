bb56d34feee7704ee2d86ec0c5698bfc9fc28c57
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-07 15:12:20 -0300

    Create  ApiCatalogoContext  e configuration sntrigConnection

---
+++ ApiCatalogo/ApiCatalogo/Context/ApiCatalogoContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiCatalogo.Models; // Importa o namespace que contém as classes de modelo.
+
+using Microsoft.EntityFrameworkCore; // Importa o namespace do Entity Framework Core para acesso ao banco de dados.
+
+namespace ApiCatalogo.Context
+{
+    public class ApiCatalogoContext : DbContext
+    {
+        public ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options) : base(options)
+        {
+            // Construtor da classe, que recebe opções de configuração do contexto.
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        // Define um DbSet (conjunto) para a entidade "Categoria" que será mapeada para o banco de dados.
+
+        public DbSet<Produto>? Produtos { get; set; }
+        // Define um DbSet (conjunto) para a entidade "Produto" que será mapeada para o banco de dados.
+    }
+}
--- ApiCatalogo/ApiCatalogo/Program.cs
+++ ApiCatalogo/ApiCatalogo/Program.cs
@@ -1,25 +1,37 @@
+using ApiCatalogo.Context; // Importa o namespace que contém o contexto de dados da aplicação.
+using Microsoft.EntityFrameworkCore; // Importa o namespace do Entity Framework Core.
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Cria uma instância do aplicativo web usando o ASP.NET Core.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Adiciona serviços ao contêiner de injeção de dependência.
+builder.Services.AddControllers(); // Adiciona o serviço de controle do MVC.
+// Saiba mais sobre a configuração do Swagger/OpenAPI em https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer(); // Adiciona suporte para a exploração de endpoints (Swagger).
+builder.Services.AddSwaggerGen(); // Configura o Swagger para geração de documentação da API.
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+// Obtém a string de conexão do arquivo de configuração "appsettings.json" com o nome "DefaultConnection".
+
+builder.Services.AddDbContext<ApiCatalogoContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+// Configura o Entity Framework Core para usar o MySQL com a string de conexão "mySqlConnection" e a versão do servidor automaticamente detectada.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura o pipeline de solicitação HTTP.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // Adiciona o middleware Swagger para a geração de documentação da API.
+    app.UseSwaggerUI(); // Adiciona o middleware Swagger UI para explorar a documentação da API no navegador.
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Redireciona as solicitações HTTP para HTTPS (SSL/TLS).
 
-app.UseAuthorization();
+app.UseAuthorization(); // Adiciona middleware de autorização.
 
-app.MapControllers();
+app.MapControllers(); // Mapeia as rotas dos controladores para as ações apropriadas.
 
-app.Run();
+app.Run(); // Inicia o aplicativo web e aguarda solicitações.
