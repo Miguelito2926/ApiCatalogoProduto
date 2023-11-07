44e52056b2c1adf2c89710e7391592dbebab2eb6
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-07 13:12:00 -0300

    Create entitie Produto

--- ApiCatalogo/ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -1,8 +1,8 @@
-﻿namespace ApiCatalogo.Properties;
+﻿namespace ApiCatalogo.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
-    public string? ImagemUrl {  get; set; }
+    public string? ImagemUrl { get; set; }
 }
