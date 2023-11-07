6f4f3234331a54d5e673c9f9be494f05180bf3f7
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-07 15:37:58 -0300

    Defining relationships between entities in the database

--- ApiCatalogo/ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -1,8 +1,19 @@
-﻿namespace ApiCatalogo.Models;
+﻿using ApiCatalogo.Models;
+using System.Collections.ObjectModel; // Importa o namespace para a coleção ObservableCollection.
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
+    // Propriedade que representa o ID único da categoria.
+
     public string? Nome { get; set; }
+    // Propriedade que representa o nome da categoria.
+
     public string? ImagemUrl { get; set; }
+    // Propriedade que representa a URL da imagem da categoria.
+
+    public ICollection<Produto>? Produtos { get; set; } = new Collection<Produto>();
+    // Propriedade de navegação que representa uma coleção de produtos associados a esta categoria.
+    // Inicializa a coleção de produtos como uma nova instância de Collection<Produto> por padrão.
 }
+
--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -3,11 +3,29 @@
 public class Produto
 {
     public int ProdutoId { get; set; }
-    public string? Nome { get; set; }
-    public string? Descricao { get; set; }
-    public decimal Preco {  get; set; }
-    public string? ImagemUrl { get; set;}
-    public float Estoque { get; set; }
-    public DateTime DataCadastro { get; set;}
+    // Propriedade que representa o ID único do produto.
 
+    public string? Nome { get; set; }
+    // Propriedade que representa o nome do produto.
+
+    public string? Descricao { get; set; }
+    // Propriedade que representa a descrição do produto.
+
+    public decimal Preco { get; set; }
+    // Propriedade que representa o preço do produto.
+
+    public string? ImagemUrl { get; set; }
+    // Propriedade que representa a URL da imagem do produto.
+
+    public float Estoque { get; set; }
+    // Propriedade que representa a quantidade em estoque do produto.
+
+    public DateTime DataCadastro { get; set; }
+    // Propriedade que representa a data de cadastro do produto.
+
+    public int CategoriaId { get; set; }
+    // Propriedade que representa o ID da categoria do produto.
+
+    public Categoria? Categoria { get; set; }
+    // Propriedade de navegação que representa a categoria associada ao produto.
 }
