3b5b1574edbe2d864eea5413c2077543ae541178
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-08 12:53:26 -0300

    Create Action List Produtos

--- ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,26 @@ namespace ApiCatalogo.Controllers
 
         public ProdutosController(ApiCatalogoContext context)
         {
-            _context=context;
+            _context = context;
+            // Inicializa o controleador com o contexto do banco de dados.
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            // Método que responde a solicitações HTTP GET para listar produtos.
+
+            var produtos = _context.Produtos.ToList();
+            // Obtém todos os produtos do banco de dados.
+
+            if (produtos is null)
+            {
+                return NotFound("Recurso não encontrado.");
+                // Retorna um código de resposta "404 Not Found" com uma mensagem se não houver produtos.
+            }
+
+            return produtos;
+            // Retorna a lista de produtos como resultado da solicitação.
         }
     }
 }
