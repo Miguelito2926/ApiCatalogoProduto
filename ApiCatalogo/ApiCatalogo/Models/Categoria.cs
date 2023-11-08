b6e109211aa5fd4c34727bb487ac2c8bb9f8232b
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-07 21:53:38 -0300

    Create database e migrations

--- ApiCatalogo/ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -1,14 +1,23 @@
 ﻿using ApiCatalogo.Models;
 using System.Collections.ObjectModel; // Importa o namespace para a coleção ObservableCollection.
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+[Table("Categorias")]
 public class Categoria
 {
+    [Key] // Defini a chave Primaria
     public int CategoriaId { get; set; }
     // Propriedade que representa o ID único da categoria.
 
+    
+    [Required] // Especifica que o valor do campo é obrigatório
+    [MaxLength(80)]//Defini o tamanho mínimo ou máximo permitido para o tipo
     public string? Nome { get; set; }
     // Propriedade que representa o nome da categoria.
 
+    [Required] 
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
     // Propriedade que representa a URL da imagem da categoria.
 
--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -1,31 +1,43 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiCatalogo.Models;
+[Table("Produtos")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
     // Propriedade que representa o ID único do produto.
 
+    [Required] // Especifica que o valor do campo é obrigatório
+    [MaxLength(80)] // Defini o tamanho minimo ou maximo permitido para o tipo
     public string? Nome { get; set; }
     // Propriedade que representa o nome do produto.
 
+    [Required]
+    [MaxLength(300)]
     public string? Descricao { get; set; }
     // Propriedade que representa a descrição do produto.
 
+    [Required]
+    [Column(TypeName ="decimal(10,2)")]
     public decimal Preco { get; set; }
     // Propriedade que representa o preço do produto.
 
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     // Propriedade que representa a URL da imagem do produto.
 
     public float Estoque { get; set; }
     // Propriedade que representa a quantidade em estoque do produto.
-
+   
     public DateTime DataCadastro { get; set; }
     // Propriedade que representa a data de cadastro do produto.
-
+   
     public int CategoriaId { get; set; }
     // Propriedade que representa o ID da categoria do produto.
-
+    
     public Categoria? Categoria { get; set; }
     // Propriedade de navegação que representa a categoria associada ao produto.
 }
