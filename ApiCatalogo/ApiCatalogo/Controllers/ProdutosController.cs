41205719e95ec6989e84d3352039c5329a30a403
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-08 16:08:03 -0300

    Create Actions POST, PUT E DELETE.

--- ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -33,6 +34,58 @@ namespace ApiCatalogo.Controllers
 
             return produtos;
             // Retorna a lista de produtos como resultado da solicitação.
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Recurso não encontrado.");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto",
+            new { id = produto.ProdutoId }, produto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest("Campos obrigatórios de entrada não enviados ou erros de validação dos campos de entrada.");
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            // produto = _context.Produtos.Find(id);
+            if(produto is null)
+            {
+                return NotFound("Recurso não encontrado.");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges(); 
+            return Ok(produto);
         }
     }
 }
