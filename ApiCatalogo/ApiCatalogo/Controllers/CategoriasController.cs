d2bfd05bc7e2498883627282df6210e03d9a9823
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-14 12:51:38 -0300

    Fix endpoint ObterCategoria por ID

--- ApiCatalogo/ApiCatalogo/Controllers/CategoriasController.cs
+++ ApiCatalogo/ApiCatalogo/Controllers/CategoriasController.cs
@@ -1,39 +1,45 @@
-﻿using ApiCatalogo.Context;
+﻿// Importando namespaces necessários
+using ApiCatalogo.Context;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+// Definindo o namespace e declarando a classe do controlador CategoriasController
 namespace ApiCatalogo.Controllers
 {
     [Route("[controller]")]
     [ApiController]
     public class CategoriasController : ControllerBase
     {
+        // Declaração do contexto do banco de dados
         private readonly ApiCatalogoContext _context;
 
+        // Construtor que recebe o contexto do banco de dados como parâmetro
         public CategoriasController(ApiCatalogoContext context)
         {
             _context = context;
-            
         }
 
+        // Endpoint para obter todas as categorias com seus produtos
         [HttpGet("produtos")]
         public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
         {
             try
             {
                 // Exemplo de método sem filtro, não é uma boa prática
-                // return _context.Categorias.Include(p => p.Produtos).ToList(); // Exemplo de método com filtro, boa prática de consulta de lista
+                // return _context.Categorias.Include(p => p.Produtos).ToList();
+                // Exemplo de método com filtro, boa prática de consulta de lista
                 return _context.Categorias.Include(p => p.Produtos).Where(c => c.CategoriaId <= 10).ToList();
             }
             catch (Exception)
             {
-
+                // Retornando um erro interno do servidor em caso de exceção
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     "Internal Server Error. Solicitação não enviada. Precisa ser executada novamente.");
             }
         }
 
+        // Endpoint para obter todas as categorias
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> GetAll()
         {
@@ -41,7 +47,8 @@ namespace ApiCatalogo.Controllers
             return _context.Categorias.AsNoTracking().ToList();
         }
 
-        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        // Endpoint para obter uma categoria por ID usando restrição de rota definindo que espera um ID do tipo inteiro maior que 0
+        [HttpGet("{id:int:min(1)}", Name = "ObterCategoria")]
         public ActionResult<Categoria> Get(int id)
         {
             try
@@ -53,22 +60,23 @@ namespace ApiCatalogo.Controllers
                     return NotFound("Recurso não encontrado.");
                 }
 
+                // Retornando Ok com a categoria encontrada
                 return Ok(categoria);
             }
-            catch (Exception)   
+            catch (Exception)
             {
+                // Retornando um erro interno do servidor em caso de exceção
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     "Internal Server Error. Solicitação não enviada. Precisa ser executada novamente.");
             }
-            
         }
 
+        // Endpoint para adicionar uma nova categoria
         [HttpPost]
         public ActionResult Post(Categoria categoria)
         {
             try
             {
-
                 if (categoria is null)
                 {
                     // Retornando BadRequest com uma mensagem personalizada
@@ -82,11 +90,13 @@ namespace ApiCatalogo.Controllers
             }
             catch (Exception)
             {
+                // Retornando um erro interno do servidor em caso de exceção
                 return StatusCode(StatusCodes.Status500InternalServerError,
                         "Internal Server Error. Solicitação não enviada. Precisa ser executada novamente.");
             }
         }
 
+        // Endpoint para atualizar uma categoria existente
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Categoria categoria)
         {
@@ -102,6 +112,7 @@ namespace ApiCatalogo.Controllers
             return Ok(categoria);
         }
 
+        // Endpoint para excluir uma categoria
         [HttpDelete("{id:int}")]
         public ActionResult<Categoria> Delete(int id)
         {
@@ -125,7 +136,6 @@ namespace ApiCatalogo.Controllers
                 // Retornando BadRequest com uma mensagem personalizada
                 return BadRequest("Bad Request. Campos obrigatórios de entrada não enviados ou erros de validação dos campos de entrada.");
             }
-           
         }
     }
 }
